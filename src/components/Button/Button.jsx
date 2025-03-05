977113a4cc488975e6e224b8d7157c89eed46a2d
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-03-05 15:21:07 +0100

    Creating a button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,25 @@
-const Button = () => {
+import "./button.css"
+
+// const Button = ({ onClick, children, as: Component = 'button', ...rest }) => {
+const Button = ({ onClick, text, as: Component = 'button', ...rest }) => {
 
     return (
-        <></>
-    )
-}
+        // <Component onClick={onClick} className="lh-btn" {...rest}>
+        //     {children}
+        // </Component>
+
+        <Component onClick={onClick} className="button-container-2 test-animation lh-btn" {...rest}>
+            <span class="mas">{text}</span>
+            <button type="button" name="Hover">{text}</button>
+        </Component>
+    );
+};
 
 export default Button
+
+/*
+<div class="button-container-2">
+    <span class="mas">MASK2</span>
+    <button type="button" name="Hover">MASK2</button>
+</div>
+*/
