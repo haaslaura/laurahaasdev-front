7b8830d4e3861f5ba6dc3b14aa09c11c5878761a
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-03-07 16:44:36 +0100

    Create skills page

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,7 +1,28 @@
+import "./skills.css"
+import skillsList from "../../data/list-skills.json"
+import SkillElement from "../../components/SkillElement/SkillElement"
+import PageTitle from "../../components/PageTitle/PageTitle";
+
+
+
 const Skills = () => {
 
+    console.log(skillsList);
+    
+
     return (
-        <></>
+        <section id="skills-section">
+            <PageTitle text="Compétences" classColorName="dark-h1" />
+            <div id="skills-section__list">
+                {skillsList.map((skill) => (
+                    <SkillElement
+                        key={skill.id}
+                        text={skill.name}
+                        icon={skill.icon}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
 
