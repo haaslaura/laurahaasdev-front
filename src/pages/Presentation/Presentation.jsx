9b31c8c330a3b0b469e8e0aff9f27372baf611b9
haaslaura <110221401+haaslaura@users.noreply.github.com>
2025-03-07 16:46:27 +0100

    Minor corrections

--- src/pages/Presentation/Presentation.jsx
+++ src/pages/Presentation/Presentation.jsx
@@ -30,29 +30,31 @@ const Presentation = () => {
             
             <div id="presentation__content">
                 <img src={profilePic} alt="" />
-                <p>
-                    Passionnée par la technologie depuis l'enfance, je termine en 2025 un parcours de reconversion pour devenir développeuse. Un vrai challenge, après 10&nbsp;ans à accompagner des entreprises dans leur communication et leur marketing !
-                </p>
-                <p>
-                    J'aime la dimension technique et la logique sous-jacente des projets informatiques, mais aussi le lien créé avec les utilisateurs : ce sont eux qui sont au cœur de nos réflexions et de nos objectifs.
-                </p>
-                <p>
-                    Mon parcours m'a conduite à conjuguer créativité et technique. Soucieuse de l'accessibilité de mes réalisations, je souhaite mettre mes compétences au service d'une équipe dynamique et de projets porteurs de sens.
-                </p>
-                <p>
-                    Mon objectif aujourd'hui : progresser continuellement pour vous aider à anticiper les enjeux technologiques de demain !
-                </p>
-                <div id="presentation__btn">
-                    <Button
-                        as={Link}
-                        to="/contact"
-                        text="Prenons contact"
-                    />
-                    <Button
-                        as={Link}
-                        to="/competences"
-                        text="Mes compétences"
-                    />
+                <div id="presentation__text">
+                    <p>
+                        Passionnée par la technologie depuis l'enfance, je termine en 2025 un parcours de reconversion pour devenir développeuse. Un vrai challenge, après 10&nbsp;ans à accompagner des entreprises dans leur communication et leur marketing !
+                    </p>
+                    <p>
+                        J'aime la dimension technique et la logique sous-jacente des projets informatiques, mais aussi le lien créé avec les utilisateurs : ce sont eux qui sont au cœur de nos réflexions et de nos objectifs.
+                    </p>
+                    <p>
+                        Mon parcours m'a conduite à conjuguer créativité et technique. Soucieuse de l'accessibilité de mes réalisations, je souhaite mettre mes compétences au service d'une équipe dynamique et de projets porteurs de sens.
+                    </p>
+                    <p>
+                        Mon objectif aujourd'hui : progresser continuellement pour vous aider à anticiper les enjeux technologiques de demain !
+                    </p>
+                    <div id="presentation__btn">
+                        <Button
+                            as={Link}
+                            to="/contact"
+                            text="Prenons contact"
+                        />
+                        <Button
+                            as={Link}
+                            to="/competences"
+                            text="Mes compétences"
+                        />
+                    </div>
                 </div>
             </div>
         </section>
